1b48980ceddf6d0c0123a18767286f693ede5daf
sandhyachinnan <1nc23cs037@ncetmail.com>
2026-07-10 19:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (20.44%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n=accounts.size(); 
+        int m=accounts[0].size();
+        int maxx=INT_MIN; 
+        for(int i=0;i<n;i++)
+        { 
+            int sum=0;
+            for(int j=0;j<m;j++)
+            {
+                sum+=accounts[i][j];
+
+            }
+            maxx=max(maxx,sum);
+        }
+        return maxx;
+        
+    }
+};
