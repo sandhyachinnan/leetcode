74fb1f4011341f952f16f75479e35bf0da9c43c7
sandhyachinnan <1nc23cs037@ncetmail.com>
2026-07-17 10:52:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (17.57%) - LeetSync

---
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+        int c=0;
+        int maxx=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='E')
+            {
+                c++;
+            }
+            else
+            {
+                c--;
+            }
+            maxx=max(maxx,c);
+        }
+        return maxx;
+          
+    }
+
+};
