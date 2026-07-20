e87b8274dd5a89e97406ebe6416c658546cbbe20
sandhyachinnan <1nc23cs037@ncetmail.com>
2026-07-20 11:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (76.12%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string a="";
+        for(int i=0;i<address.size();i++)
+        {
+            if(address[i]=='.')
+            {
+                a+="[.]";
+            }
+            else
+            {
+                a+=address[i];
+
+            }
+        }
+        return a;
+        
+    }
+};
