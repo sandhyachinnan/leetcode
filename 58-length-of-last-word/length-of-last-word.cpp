474827a8e6964602b608043c2d3b87e70424aa4a
sandhyachinnan <1nc23cs037@ncetmail.com>
2026-09-02 18:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (92.63%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution { 
+public: 
+    int lengthOfLastWord(string s) { 
+        int c = 0; 
+
+        for(int i = s.size()-1; i >= 0; i--) { 
+            
+            if(s[i] == ' ' && c == 0) { 
+                continue; 
+            }
+            else if(s[i] == ' ') { 
+                break; 
+            }
+            else { 
+                c++; 
+            } 
+        }  
+
+        return c;  
+    } 
+};
